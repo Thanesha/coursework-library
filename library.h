0594dc286e3270f0fa6e075338c602811e0af338
Thanesha Hemrage <TH660@live.mdx.ac.uk>
2024-01-08 20:51:26 +0400

    changes in methods in class library

---
+++ library.h
@@ -0,0 +1,118 @@
+#ifndef _LIBRARY_H_ 
+#define _LIBRARY_H_ 
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <map>
+using namespace std;
+class Person {
+private:
+    std::string name;
+    std::string address;
+    std::string email;
+
+public:
+    Person(std::string name, std::string address, std::string email) : name(name), address(address), email(email) {}
+
+    std::string getName() { return name; }
+    void setName(std::string name) { this->name = name; }
+
+    std::string getAddress() { return address; }
+    void setAddress(std::string address) { this->address = address; }
+
+    std::string getEmail() { return email; }
+    void setEmail(std::string email) { this->email = email; }
+};
+class Member : public Person 
+{
+private:
+    int memberID;
+    std::vector<Book> booksLoaned;
+
+public:
+    Member(int memberID, std::string name, std::string address, std::string email) : Person(name, address, email), memberID(memberID) {}
+
+    int getMemberID() { return memberID; }
+        void addMember() {}
+        void issueBook(int bookID, int memberID) {}
+        void returnBook(int bookID, int memberID) {}
+        void displayBorrowedBooks() {}
+        void calcFine(int memberID) {}
+
+    std::vector<Book> getBooksBorrowed() { return booksLoaned; }
+    void setBooksBorrowed(Book book) { booksLoaned.push_back(book); }
+
+    void returnBook(Book book) {
+        for (int i = 0; i < booksLoaned.size(); i++) {
+            if (booksLoaned[i].getBookID() == book.getBookID()) {
+                booksLoaned.erase(booksLoaned.begin() + i);
+                break;
+            }
+        }
+    }
+
+    void displayBorrowedBooks() {
+        std::cout << "Member ID: " << memberID << "\n";
+        std::cout << "Name: " << name << "\n";
+        std::cout << "Address: " << address << "\n";
+        std::cout << "Email: " << email << "\n";
+        std::cout << "Books Borrowed:\n";
+        for (Book book : booksLoaned) {
+            std::cout << book.getBookName() << "\n";
+        }
+    }
+
+    void calcFine() {
+        time_t currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        for (Book book : booksLoaned) {
+            std::tm dueDateTm = book.getDueDate();
+            time_t dueTime = std::mktime(&dueDateTm);
+            double fine = std::difftime(currentTime, dueTime) / (60 * 60 * 24);
+            if (fine > 0) {
+                std::cout << "Fine for book: " << book.getBookName() << " is $" << fine << "\n";
+            }
+        }
+    }
+};
+class Librarian : public Person
+{
+    private:
+        int staffID;
+        int salary;
+    public:
+        Librarian(int staffID, std::string name, std::string address, std::string email, int salary);
+
+        int getStaffID();
+        void setStaffID(int staffID);
+
+        int getSalary();
+        void setSalary(int salary);
+};
+class Book : public Member
+{
+private:
+    int bookID;
+    std::string bookName;
+    std::string authorFirstName;
+    std::string authorLastName;
+    std::string bookType;
+    std::tm dueDate;
+
+public:
+    Book(int bookID, std::string bookName, std::string authorFirstName, std::string authorLastName) : bookID(bookID), bookName(bookName), authorFirstName(authorFirstName), 
+     authorLastName(authorLastName) {}
+    borrowerBook()
+    int getBookID() { return bookID; }
+    std::string getBookName() { return bookName; }
+
+    std::string getAuthorFirstName() { return authorFirstName; }
+    std::string getAuthorLastName() { return authorLastName; }
+
+    std::tm getDueDate() { return dueDate; }
+    void setDueDate(std::tm dueDate) { this->dueDate = dueDate; }
+};
+
+
+#endif
